12c7ed7397138e6e8dc2eb629923968aac22b99e
Gilfan04 <muhgilfani@gmail.com>
2025-07-17 15:56:17 +0700

    Create background.py

---
+++ background.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import base64
+
+def set_background(image_path):
+    with open(image_path, "rb") as f:
+        data = f.read()
+    encoded = base64.b64encode(data).decode()
+
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image: url("data:image/png;base64,{encoded}");
+            background-size: cover;
+            background-attachment: fixed;
+            background-repeat: no-repeat;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
